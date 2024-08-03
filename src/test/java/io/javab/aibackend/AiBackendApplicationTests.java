a25fdf070b22772c5da63ca252302fa61942a400
Ajay Madkaiker <129883457+madkaikerA@users.noreply.github.com>
2024-08-03 14:09:38 -0500

    Basic Initializr working state

---
+++ src/test/java/io/javab/aibackend/AiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javab.aibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
